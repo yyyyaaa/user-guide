144c7ebcb83c014c3ad75bf814b72a12fd03e3bd
phatg <phatghaa@gmail.com>
2019-02-01 21:21:16 +0700

    fix: remove unused links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,9 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'script-bundle', label: 'Docs'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {doc: 'script-bundle', label: 'Docs'}
   ],
 
   // If you have users set above, you add it here:
